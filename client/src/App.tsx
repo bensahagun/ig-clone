a2fbcca477905bedf1decfa194c696ef6cacfe19
Ben Sahagun <benedicksahagun@gmail.com>
2021-09-27 23:47:05 +0800

    feat(client-auth): added basic signin signout

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,17 +1,12 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import { SignIn } from './components/SignIn';
+import { AuthProvider } from './firebase/context';
 
 function App() {
   return (
-    <div className='App'>
-      <header className='App-header'>
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a className='App-link' href='https://reactjs.org' target='_blank' rel='noopener noreferrer'>
-          Learn React
-        </a>
-      </header>
-    </div>
+    <AuthProvider>
+      <SignIn />
+    </AuthProvider>
   );
 }
 
